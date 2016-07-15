090872530da781aac87bd727c997bc7546ae8be4
Chaelgutierrez <leeez711@gmail.com>
2016-07-15 10:50:59 +0800

    add

---
+++ keyascii.php
@@ -0,0 +1,33 @@
+
+<?php
+	
+	$t9encrypt='$t9encrypt';
+	
+	$t=127;
+	$r=29;
+
+	do{
+	$t=$t+1;
+
+	echo /*"$t9encrypt=(chr(",$t,"),);",*/chr($t);
+	echo "</br></br>";
+
+	}while($t<255);
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
