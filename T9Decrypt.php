678335c1a64931051e1d55eb63ebd0e954c2674c
Chaelgutierrez <leeez711@gmail.com>
2016-07-15 10:47:29 +0800

    added

---
+++ T9Decrypt.php
@@ -0,0 +1,512 @@
+<?php
+	global $t9decrypt;//temporary usage of global scope of variable to avoid errors in loading this php file  
+	//t9decrypt standard variable with its corresponding value
+	
+	global $key1;global $key2;global $key3;global $key4;global $key5;global $key6;global $key7;global $key8;global $key9;//global usage for keys values
+	$key1=1;$key2=2;$key3=3;$key4=4;$key5=5;$key6=6;$key7=7;$key8=8;$key9=9;//key_values
+	
+
+	//Priority 1
+
+	
+	
+	$t9decrypt=" ".$t9decrypt;
+	
+	//Key 2
+	
+	$t9decrypt=str_replace(" ".$key2."-25",chr(231),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-24",chr(230),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-23",chr(229),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-22",chr(228),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-21",chr(227),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-20",chr(226),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-19",chr(225),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-18",chr(224),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-17",chr(223),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-16",chr(222),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-15",chr(199),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-14",chr(198),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-13",chr(197),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-12",chr(196),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-11",chr(195),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-10",chr(194),$t9decrypt);//
+	
+	$t9decrypt=str_replace(" ".$key2."-9",chr(193),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key2."-8",chr(192),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-7",chr(140),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-6","a",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-5","A",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-4","b",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-3","B",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-2","c",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key2."-1","C",$t9decrypt);
+	
+	
+	
+	//Key 3
+	
+	$t9decrypt=str_replace(" ".$key3."-15",chr(235),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-14",chr(234),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key3."-13",chr(233),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-12",chr(232),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-11",chr(208),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key3."-10",chr(203),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-9",chr(202),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-8",chr(201),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key3."-7",chr(200),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-6","d",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-5","D",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-4","e",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-3","E",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key3."-2","f",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key3."-1","F",$t9decrypt);
+	
+	
+	
+	// Key 4
+	
+	$t9decrypt=str_replace(" ".$key4."-14",chr(239),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-13",chr(238),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-12",chr(237),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key4."-11",chr(236),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-10",chr(207),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-9",chr(206),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-8",chr(205),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key4."-7",chr(204),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-6","g",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-5","G",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key4."-4","h",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-3","H",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key4."-2","i",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key4."-1","I",$t9decrypt);
+	
+	
+	
+	//Key 5
+	
+	$t9decrypt=str_replace(" ".$key5."-6","j",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key5."-5","J",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key5."-4","k",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key5."-3","K",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key5."-2","l",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key5."-1","L",$t9decrypt);
+	
+	
+	
+	// Key 6
+	
+	$t9decrypt=str_replace(" ".$key6."-23",chr(248),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-22",chr(247),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-21",chr(246),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-20",chr(245),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-19",chr(244),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-18",chr(243),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-17",chr(242),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-16",chr(241),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-15",chr(240),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-14",chr(216),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-13",chr(215),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-12",chr(214),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-11",chr(213),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-10",chr(212),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-9",chr(211),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-8",chr(210),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-7",chr(209),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-6","m",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-5","M",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-4","n",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-3","N",$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key6."-2","o",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key6."-1","O",$t9decrypt);
+	
+	
+	
+	// Key 7
+	
+	$t9decrypt=str_replace(" ".$key7."-9",chr(138),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-8","p",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-7","P",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-6","q",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-5","Q",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-4","r",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-3","R",$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key7."-2","s",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key7."-1","S",$t9decrypt);
+	
+	
+	
+	//Key 8
+	
+	$t9decrypt=str_replace(" ".$key8."-14",chr(252),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-13",chr(251),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-12",chr(250),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-11",chr(249),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-10",chr(220),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-9",chr(219),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-8",chr(218),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-7",chr(217),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-6","t",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-5","T",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-4","u",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-3","U",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-2","v",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key8."-1","V",$t9decrypt);
+	
+	
+	
+	// Key 9
+	
+	$t9decrypt=str_replace(" ".$key9."-15",chr(255),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-14",chr(254),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key9."-13",chr(253),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-12",chr(221),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-11",chr(159),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key9."-10",chr(158),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-9",chr(142),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key9."-8","w",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-7","W",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-6","x",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-5","X",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key9."-4","y",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-3","Y",$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key9."-2","z",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key9."-1","Z",$t9decrypt);
+	
+	
+	
+
+
+	
+	//Priority 2
+	
+	//EXTENDED KEY ASCII
+
+	$t9decrypt=str_replace(" ".$key1."-82",chr(191),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-81",chr(190),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-80",chr(189),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-79",chr(188),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-78",chr(187),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-77",chr(186),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-76",chr(185),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-75",chr(184),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-74",chr(183),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-73",chr(182),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-72",chr(181),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-71",chr(180),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-70",chr(179),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-69",chr(178),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-67",chr(176),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-68",chr(177),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-66",chr(175),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-65",chr(174),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-64",chr(172),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-63",chr(171),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-62",chr(170),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-61",chr(169),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-60",chr(168),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-59",chr(167),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-58",chr(166),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-57",chr(165),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-56",chr(164),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-55",chr(163),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-54",chr(162),$t9decrypt);	
+
+	$t9decrypt=str_replace(" ".$key1."-53",chr(161),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-52",chr(156),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-51",chr(155),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-50",chr(154),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-49",chr(153),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-48",chr(152),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-47",chr(151),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-46",chr(150),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-45",chr(149),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-44",chr(148),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-43",chr(147),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-42",chr(146),$t9decrypt);
+
+	$t9decrypt=str_replace(" ".$key1."-41",chr(145),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-40",chr(139),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-39",chr(137),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-38",chr(136),$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-37",chr(135),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-36",chr(134),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-35",chr(133),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-34",chr(132),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-33",chr(131),$t9decrypt);	
+	
+	$t9decrypt=str_replace(" ".$key1."-32",chr(130),$t9decrypt);		
+	
+	$t9decrypt=str_replace(" ".$key1."-31",chr(128),$t9decrypt);
+		
+	
+	
+	//Special Characters 
+
+	$t9decrypt=str_replace(" ".$key1."-30","@",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-29",chr(92),$t9decrypt);// exception for  /forward slash
+	
+	$t9decrypt=str_replace(" ".$key1."-28","^",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-27","|",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-26","~",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-25","{",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-24","}",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-23","`",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-22","[",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-21","]",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-20","?",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-19","=",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-18",">",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-17","<",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-16",";",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-15",":",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-14","/",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-13",".",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-12",",",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-11","+",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-10","*",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-9",")",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-8","(",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-7","'",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-6","&",$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-5","%",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-4","$",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-3","#",$t9decrypt);
+		
+	$t9decrypt=str_replace(" ".$key1."-2",'"',$t9decrypt);
+	
+	$t9decrypt=str_replace(" ".$key1."-1","!",$t9decrypt);
+	
+
+	
+	
+
+ 
+
+
+	//Priority 3
+	$t9decrypt=str_replace(" 1","1",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 2","2",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 3","3",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 4","4",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 5","5",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 6","6",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 7","7",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 8","8",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 9","9",$t9decrypt);
+	
+	$t9decrypt=str_replace(" 0","0",$t9decrypt);
+	
+	
+	
+	//Priority 4
+	$t9decrypt=str_replace(" _"," ",$t9decrypt);
+	
+	$t9decrypt=str_replace(" -","-",$t9decrypt);
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+?>
